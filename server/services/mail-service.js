710fb4e0508e0bedb5d727aefffe2381cfb7e079
chelostudy@gmail.com <chelostudy@gmail.com>
2023-06-07 22:01:19 +0300

    Отредактированы названия моделей и переменных, работаю над обновлением статуса заказа

--- server/services/mail-service.js
+++ server/services/mail-service.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer')
+const ApiError = require("../exceptions/api-error");
 
 class MailService{
     constructor() {
@@ -12,23 +13,24 @@ class MailService{
             }
         })
     }
-
     async sendNotificationToAdmins(){
-        console.log("sendNotificationToAdmins()")
-        await this.transporter.sendMail({
-            from: process.env.SMTP_USER,
-            to: process.env.ADMIN_EMAIL,
-            subject: 'Регистрация новой заявки',
-            text: '',
-            html:
-                `
+        try {
+            await this.transporter.sendMail({
+                from: process.env.SMTP_USER,
+                to: process.env.ADMIN_EMAIL,
+                subject: 'Регистрация новой заявки',
+                text: '',
+                html:
+                    `
                     <div>
                     Зарегистрирована новая заявка, пройдите на <a href="${process.env.API_URL}">${process.env.API_URL}</a> для обработки заявки.
                     </div>
                 `
-            }
-        )
-        return true;
+            })
+            return true;
+        }catch (e){
+            throw ApiError.BadRequest('Отсутствуют необходимые для запроса значения',e);
+        }
     }
 }
 module.exports = new MailService();
