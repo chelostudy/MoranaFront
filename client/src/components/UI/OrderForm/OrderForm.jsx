90463153c980f4a45dc74f7090e4acadab67315b
chelostudy@gmail.com <chelostudy@gmail.com>
2023-06-16 04:11:21 +0300

    работа над клиентской частью

--- client/src/components/UI/OrderForm/OrderForm.jsx
+++ client/src/components/UI/OrderForm/OrderForm.jsx
@@ -1,22 +1,26 @@
 import React, {useState} from 'react';
 import {action} from "mobx";
 import cl from './orderForm.module.css'
+import {Link} from "react-router-dom";
 
 const OrderForm = (props) => {
     const[name, setName]=useState('');
     const[email, setEmail]=useState('');
     const[phone, setPhone]=useState('');
     const[text, setText]=useState('');
+    const[checked,setChecked]=useState(false)
+    function changeCheckbox() {
+        setChecked(!checked);
+    }
+
     return (
         <div className={cl.oForm}>
-
-
             <div><label htmlFor="nmb"><b>Номер телефона*</b></label>
                 <input
                     onChange={e => setPhone(e.target.value)}
                     value={phone}
                     type="text"
-                    placeholder='Ваше имя'
+                    placeholder='Введите номер телефона...(обязательно)'
                     name="nmb"
                 /></div>
 
@@ -25,7 +29,7 @@ const OrderForm = (props) => {
                 onChange={e => setName(e.target.value)}
                 value={name}
                 type="text"
-                placeholder='Ваш номер телефона'
+                placeholder='Введите ваше имя...'
                 name="nam"
             /></div>
 
@@ -38,16 +42,20 @@ const OrderForm = (props) => {
                 name="ema"
             /></div>
 
-            <div><label htmlFor="text"><b>Текст</b></label>
-            <input
+            <div style={{height: "150px"}}><label htmlFor="text"><b>Текст</b></label>
+            <textarea
+
                 onChange={e => setText(e.target.value)}
                 value={text}
-                type="text"
                 placeholder='Текст...'
                 name="text"
             /></div>
 
+            <div className={cl.oBtnDiv}><label htmlFor="pvc"><b>Я соглашаюсь с <Link to={"privacy"}>Политикой конфиденциальности</Link></b></label>
+            <input type={"checkbox"} checked={checked} onChange={changeCheckbox} name={"pvc"}/>
+            </div>
             <button onClick={async () => {
+                if (checked=="off") return{}
                 if (!email == "" || !phone == ""){const response = await props.action(text, phone, name, email)
                     try{props.setVisible(false)}catch (e){}
                     console.log(response, "HER")
--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -4,6 +4,8 @@ import RegisterForm from "../components/RegisterForm";
 import MyModal from "../components/UI/MyModal/MyModal";
 import service from "../services/OrderService"
 import OrderForm from "../components/UI/OrderForm/OrderForm";
+import whyUs from "../img/whyUs1.png"
+import {YMaps, Map, Placemark} from '@pbe/react-yandex-maps';
 
 const About = () => {
     const [modalVisible, setModalVisible] = useState(false)
@@ -15,12 +17,55 @@ const About = () => {
             </MyModal>
 
             <div className="main_blocks first_block">
-                <div className="wrapper">
-                    <h1 style={{fontWeight:"bold"}}>Товарный бетон в Ростове-на-Дону</h1>
-                    <h2>От 3000рублей/куб</h2>
-                    <button className="order_btn" onClick={() => setModalVisible(true)}>Заказать</button>
+                <div className="first_block__wrapper">
+                    <div>
+                        <h1 style={{fontWeight:"bold"}}>Товарный бетон в Ростове-на-Дону</h1>
+                        <h2>От 3000рублей/куб</h2>
+                    </div>
+                    <div>
+                        <button className="order_btn" onClick={() => setModalVisible(true)}>Заказать</button>
+                    </div>
                 </div>
             </div>
+            <div className="main_blocks second_block">
+                <div className="wrapper">
+                    <div>
+                        <h2 style={{fontWeight:"bold"}}>Почему именно мы?</h2>
+                    </div>
+                    <div className="place">
+                        <div><img src={whyUs}/><h2>Качество ГОСТ</h2></div>
+                        <div><img src={whyUs}/><h2>Сотни довольных клиентов</h2></div>
+                        <div><img src={whyUs}/><h2>15 лет на рынке</h2></div>
+                    </div>
+                </div>
+            </div>
+            <div className="main_blocks fourth_block">
+                <div className="wrapper">
+                    <div>
+                        <h1 style={{fontWeight:"bold"}}>Товарный бетон в Ростове-на-Дону</h1>
+                        <h2>От 3000рублей/куб</h2>
+                    </div>
+                    <div>
+                        <button className="order_btn" onClick={() => setModalVisible(true)}>Заказать</button>
+                    </div>
+                </div>
+            </div>
+            <div className="main_blocks fifth_block">
+                <div className="wrapper">
+                    <OrderForm>
+
+                    </OrderForm>
+                </div>
+            </div>
+            <div className="main_blocks sixth_block">
+                <YMaps>
+                        <Map className="map" defaultState={{  center: [47.283942, 39.684083], zoom: 16 }}>
+                        <Placemark geometry={[47.283942, 39.684083]} />
+                        </Map>
+                </YMaps>
+            </div>
+
+
         </div>
     );
 };
