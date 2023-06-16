c0b172a8960c12e1b2077a8a571b53cb61e81493
chelostudy@gmail.com <chelostudy@gmail.com>
2023-06-16 07:11:58 +0300

    работа над клиентской частью

--- server/index.js
+++ server/index.js
@@ -38,8 +38,6 @@ const start = async () => {
                 app
             )
             await priceService.ff()
-
-                await priceService.fff()
             .listen(3000, () => {
                 console.log("server is running at port 3000");
             });
